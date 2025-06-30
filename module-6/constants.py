acb53336b6879eb697d660b5bad36e69dcb26143
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-06-29 23:50:53 -0500

    Renames variable for clarity
    Renames the `WATER` constant to `LAKE` to better reflect its intended use and avoid confusion with other potential water-related elements in the simulation.

--- module-6/constants.py
+++ module-6/constants.py
@@ -13,7 +13,7 @@ HEIGHT: Final[int] = 22
 TREE: Final[CellState] = 'A'
 FIRE: Final[CellState] = '@'
 EMPTY: Final[CellState] = ' '
-WATER: Final[CellState] = '~'
+LAKE: Final[CellState] = '~'
 
 # Simulation Parameters
 INITIAL_TREE_DENSITY: Final[float] = 0.20  # Amount of forest that starts with trees
