c988de55a43ea2f769bb627da12bd6f633bb3985
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-06-29 22:41:43 -0500

    Add docstring to revision .py file

--- module-6/forestfiresim_325.py
+++ module-6/forestfiresim_325.py
@@ -3,4 +3,15 @@ Name: Brittaney Perry-Morgan
 Date: Sunday, June 29th, 2025
 Assignment: Module 6.2 Forest Fire Simulation: Program and Revised Flowchart
 Purpose: A program that simulates a forest fire.
+
+This module contains the modified version of `forestfiresim.py` from Module 5.2.
+The main difference is that it now includes a lake in the center of the display.
+The lake cannot be modified once it is placed. The lake acts as a firebreak that flames
+cannot cross.
+
+Modifications include:
+    - Adding a lake roughly in the center of the display.
+    - The water feature should use a different character (not A or @) and be blue.
+    - The water feature cannot be modified once it is in place.
+        - It acts as a firebreak that flames cannot cross.
 """
