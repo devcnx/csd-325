2869669a3425ddabea4567d21c6eb9cff2300882
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-06-30 22:07:15 -0500

    Updates file paths and import statements
    Updates a file path in the weather data script to reflect the correct location.
    
    Adjusts the forest fire simulation script to use relative imports for constants, improving project structure and module resolution.

--- module_4/sitka_highs.py
+++ module_4/sitka_highs.py
@@ -20,7 +20,7 @@ from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-filename = "module-4/sitka_weather_2018_simple.csv"
+filename = "module_4/sitka_weather_2018_simple.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
