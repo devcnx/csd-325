6bf713766f695070d636bba46c3fc1aa638c4b25
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-06-30 22:03:51 -0500

    Rename module-X directories to module_X for Python compatibility

--- module_6/forestfiresim_325.py
+++ module_6/forestfiresim_325.py
@@ -19,7 +19,7 @@ import random
 import sys
 import time
 from dataclasses import dataclass, field
-from constants import (
+from .constants import (
         CellState,
         EMPTY,
         FIRE,
