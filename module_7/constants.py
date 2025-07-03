30b1498a92122ca6f4d9b21ce302886609df65aa
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-07-03 17:27:38 -0500

    feat: add constants module with city and country data

---
+++ module_7/constants.py
@@ -0,0 +1,30 @@
+"""
+Constant values for the application.
+
+This module is used to store constants for the application. Since the assignment requires
+multiple iterations of the same code, these values are stored in a separate module so
+there isn't a concern if all the code is changed or modified.
+"""
+
+CITY_COUNTRIES = [
+    {
+        "city": "Santiago",
+        "country": "Chile"
+    },
+    {
+        "city": "Sao Paulo",
+        "country": "Brazil"
+    },
+    {
+        "city": "Tokyo",
+        "country": "Japan"
+    },
+    {
+        "city": "New York",
+        "country": "United States"
+    },
+    {
+        "city": "Paris",
+        "country": "France"
+    },
+]
