25c95c3b7ee8b0be3080ac5e12848c6028bc63b4
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-07-03 18:03:55 -0500

    feat: add four new cities to CITY_COUNTRIES list and simplify dictionary formatting

--- module_7/constants.py
+++ module_7/constants.py
@@ -1,30 +1,19 @@
 """
 Constant values for the application.
 
-This module is used to store constants for the application. Since the assignment requires
-multiple iterations of the same code, these values are stored in a separate module so
-there isn't a concern if all the code is changed or modified.
+This module is used to store constants for the application. Since the assignment
+requires multiple iterations of the same code, these values are stored in a separate
+module so there isn't a concern if all the code is changed or modified.
 """
 
 CITY_COUNTRIES = [
-    {
-        "city": "Santiago",
-        "country": "Chile"
-    },
-    {
-        "city": "Sao Paulo",
-        "country": "Brazil"
-    },
-    {
-        "city": "Tokyo",
-        "country": "Japan"
-    },
-    {
-        "city": "New York",
-        "country": "United States"
-    },
-    {
-        "city": "Paris",
-        "country": "France"
-    },
+    {"city": "Santiago", "country": "Chile"},
+    {"city": "Sao Paulo", "country": "Brazil"},
+    {"city": "Tokyo", "country": "Japan"},
+    {"city": "New York", "country": "United States"},
+    {"city": "Paris", "country": "France"},
+    {"city": "London", "country": "United Kingdom"},
+    {"city": "Rome", "country": "Italy"},
+    {"city": "Moscow", "country": "Russia"},
+    {"city": "Beijing", "country": "China"},
 ]
